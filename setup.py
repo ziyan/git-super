74aec481a20f8a0f65d4520cf1f99434542d96db
Ziyan Zhou <zhou@ziyan.net>
2022-02-19 10:23:29 +0900

    Update scripts.

--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@ from distutils.core import setup
 setup(
     name='gitsuper',
     long_description='Git scripts for managing submodule trees',
-    version='0.1.0',
+    version='0.1.1',
     packages=['gitsuper'],
     license='MIT',
     scripts=[
-    	'bin/git-super-root',
-    	'bin/git-super-foreach',
+        'bin/git-super-root',
         'bin/git-super-sync',
         'bin/git-super-branch',
         'bin/git-super-merge',
         'bin/git-super-checkout',
-        'bin/git-super-pull',
     ],
 )
